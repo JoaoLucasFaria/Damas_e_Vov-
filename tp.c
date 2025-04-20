53d4ea6f282b926c580eee29b9363c6df94f370e
João Lucas de Vilas Bôas Faria <148912535+JoaoLucasFaria@users.noreply.github.com>
2025-04-20 17:29:50 -0300

    Update tp.c

--- tp.c
+++ tp.c
@@ -1,7 +1,7 @@
 #include "tp.h"
 
 int N, M;
-int tabuleiro[MAX_N][MAX_M];
+int **tabuleiro;
 int dx[4] = {-1, -1, 1, 1};
 int dy[4] = {-1, 1, -1, 1};
 
@@ -9,11 +9,27 @@ int dentro_limites(int x, int y) {
     return x >= 0 && x < N && y >= 0 && y < M;
 }
 
-void copiar_tabuleiro(int dest[MAX_N][MAX_M], int src[MAX_N][MAX_M]) {
-    for (int i = 0; i < MAX_N; i++)
-        for (int j = 0; j < MAX_M; j++)
+void copiar_tabuleiro(int **dest, int **src, int linhas, int colunas) {
+    for (int i = 0; i < linhas; i++)
+        for (int j = 0; j < colunas; j++)
             dest[i][j] = src[i][j];
 }
+
+int **alocar_tabuleiro(int linhas, int colunas) {
+    int **tab = malloc(linhas * sizeof(int *));
+    for (int i = 0; i < linhas; i++) {
+        tab[i] = malloc(colunas * sizeof(int));
+    }
+    return tab;
+}
+
+void liberar_tabuleiro(int **tab, int linhas) {
+    for (int i = 0; i < linhas; i++) {
+        free(tab[i]);
+    }
+    free(tab);
+}
+
 
 void imprimir_tempos(struct rusage *inicio, struct rusage *fim) {
     long segundos_usuario = fim->ru_utime.tv_sec - inicio->ru_utime.tv_sec;
