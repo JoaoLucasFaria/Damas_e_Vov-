9ea239a74c83cb1ece991bb8b218ff1ea1d30ef8
João Lucas de Vilas Bôas Faria <148912535+JoaoLucasFaria@users.noreply.github.com>
2025-04-20 17:29:08 -0300

    Update backtracking.c

--- backtracking.c
+++ backtracking.c
@@ -2,7 +2,7 @@
 
 int melhor_solucao = 0;
 
-void capturar_backtracking(int tab[MAX_N][MAX_M], int x, int y, int capturas_atuais)
+void capturar_backtracking(int **tab, int x, int y, int capturas_atuais)
 {
     int captura_feita = 0;
 
@@ -17,14 +17,12 @@ void capturar_backtracking(int tab[MAX_N][MAX_M], int x, int y, int capturas_atu
         {
             captura_feita = 1;
 
-            // Realiza jogada diretamente no tabuleiro atual
             tab[x][y] = 0;
             tab[nx][ny] = 0;
             tab[nx2][ny2] = 1;
 
             capturar_backtracking(tab, nx2, ny2, capturas_atuais + 1);
 
-            // Backtrack
             tab[x][y] = 1;
             tab[nx][ny] = 2;
             tab[nx2][ny2] = 0;
@@ -42,24 +40,24 @@ int backtracking(FILE *fp, FILE *out)
     while (fscanf(fp, "%d %d", &N, &M) == 2 && (N != 0 || M != 0))
     {
         melhor_solucao = 0;
+        tabuleiro = alocar_tabuleiro(N, M);
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
                 tabuleiro[i][j] = ((i + j) % 2 == 0) ? ({ int v; fscanf(fp, "%d", &v); v; }) : 0;
-            }
-        }
 
-        // Para cada peça do jogador, inicia a busca com backtracking
         for (int i = 0; i < N; i++)
             for (int j = 0; j < M; j++)
                 if (tabuleiro[i][j] == 1)
                 {
-                    int copia[MAX_N][MAX_M];
-                    copiar_tabuleiro(copia, tabuleiro);
+                    int **copia = alocar_tabuleiro(N, M);
+                    copiar_tabuleiro(copia, tabuleiro, N, M);
                     capturar_backtracking(copia, i, j, 0);
+                    liberar_tabuleiro(copia, N);
                 }
 
         fprintf(out, "%d\n", melhor_solucao);
+        liberar_tabuleiro(tabuleiro, N);
     }
 
     return 0;
